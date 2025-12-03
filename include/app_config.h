2ffdf854a703df6cff5760579246034f0c199774
Jacob <jacobgutierrez752@gmail.com>
2025-12-03 10:59:04 -0800

    Add configs for camera service

--- include/app_config.h
+++ include/app_config.h
@@ -1,8 +1,29 @@
 #ifndef __APP_CONFIG_H
 #define __APP_CONFIG_H
 
+/**
+ * @brief Application UART baud rate for UART to USB comm
+ * 
+ */
 #define CONFIG_BAUD_RATE (PIO_CONFIG_BAUD_RATE)
 
-#define CONFIG_VIDEO_FRAME_RATE (10)
+/**
+ * @brief Maximum allowable framerate for the camera
+ * 
+ */
+#define CONFIG_VIDEO_FRAME_RATE (100)
+
+/**
+ * @brief Root of camera frame buffers
+ * 
+ */
+#define CAMERA_FB_ROOT "/camera"
+
+/**
+ * @brief Number of seconds to store *prior*
+ * to an event happening
+ * 
+ */
+#define CAMERA_FB_SECOND_RANGE (30)
 
 #endif  // __APP_CONFIG_H
