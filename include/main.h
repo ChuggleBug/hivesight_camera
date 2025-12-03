386e963a50801a88c53f184c985b5638202b7e4e
Jacob <jacobgutierrez752@gmail.com>
2025-12-03 10:58:07 -0800

    Update main to new config structure

--- include/main.h
+++ include/main.h
@@ -2,24 +2,76 @@
 #define __MAIN_H
 
 #include <Arduino.h>
-#include <Preferences.h>
+#include <FS.h>  // FS class interface
+#include <HTTPClient.h>
+#include <LittleFS.h>  // Flash Memory FS
+#include <NTPClient.h>
+#include <PubSubClient.h>
+#include <SD_MMC.h>  // SD Card Driver
 #include <WiFi.h>
+#include <WiFiUdp.h>
 
 #include "app_config.h"
 
-// Destination of MQTT Broker
-// IP adress to be configured via WiFi Manager
-extern IPAddress coordinatorIP;
-extern const uint16_t brokerPort;
-extern const uint16_t httpPort;
+#define DEVICE_TYPE "camera"
 
-// Name of the sensor defiend with WiFi Manager
-// Will be used when publishing a topic
+/**
+ * @brief Wi-Fi SSID to use when connecting to internet
+ *
+ */
+extern String wifiSSID;
+
+/**
+ * @brief Wi-Fi password to use when connecting to internet
+ * @note This value gets cleared after connected to wifif
+ */
+extern String wifiPass;
+
+/**
+ * @brief Name of the deivce. This name will be used
+ * inside of the network
+ *
+ */
 extern String deviceName;
 
-// Storage for options that are not automatically
-// stored by WiFi Manager
-extern Preferences prefs;
+/**
+ * @brief IP address of the MQTT broker
+ * @note This is typically the same as `coordinatorIP`
+ *
+ */
+extern IPAddress brokerIP;
+/**
+ * @brief Port to use for the MQTT broker
+ *
+ */
+extern uint16_t brokerPort;
 
+/**
+ * @brief IP address of the coordinator HTTP endpoint
+ * @note This is typically the same as `brokerIP`
+ */
+extern IPAddress coordinatorIP;
+/**
+ * @brief Port to use for the coordinator HTTP endpoint
+ *
+ */
+extern uint16_t coordinatorPort;
 
-#endif // __MAIN_H
+/**
+ * @brief Set of network interfaces avalible for each container
+ *
+ */
+
+extern WiFiClient netif;
+extern WiFiUDP netifUDP;
+
+/**
+ * @brief Set of application interfaces avalible for each container
+ *
+ */
+
+extern PubSubClient mqttClient;
+extern NTPClient timeClient;
+extern HTTPClient http;
+
+#endif  // __MAIN_H
